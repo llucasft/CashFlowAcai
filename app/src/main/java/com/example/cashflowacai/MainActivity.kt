e8e74fea4946f22e4097054fa67f541f20d9a195
Lucas Ferreira <lucasfteo@gmail.com>
2022-10-16 14:39:21 -0300

    Setting the interface first layout

--- app/src/main/java/com/example/cashflowacai/MainActivity.kt
+++ app/src/main/java/com/example/cashflowacai/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.cashflowacai
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var tvDate : TextView? = null
+    var btnDate : Button? = null
+    val calendar = Calendar.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvDate = findViewById(R.id.tvDate)
+        btnDate = findViewById(R.id.btnDate)
+
+        dateUpdateInView()
+    }
+
+    private fun dateUpdateInView(){
+        val dateFormatToPtBr = "dd/MM/yyyy"
+        val simpleDateFormat = SimpleDateFormat(dateFormatToPtBr, Locale("pt", "br"))
+        tvDate!!.text = simpleDateFormat.format(calendar.time)
     }
 }
