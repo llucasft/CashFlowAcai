e621d7188eb222fee9c4544c6a3333b2b8bd2b8e
Lucas Ferreira <lucasfteo@gmail.com>
2022-10-20 18:32:37 -0300

    Created Query function to access db registers.

--- app/src/main/java/com/example/cashflowacai/database/dao/RegisterDao.kt
+++ app/src/main/java/com/example/cashflowacai/database/dao/RegisterDao.kt
@@ -3,6 +3,7 @@ package com.example.cashflowacai.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.cashflowacai.model.Register
 
 @Dao
@@ -10,4 +11,7 @@ interface RegisterDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun save(vararg register: Register)
+
+    @Query("SELECT * FROM Register")
+    fun selectFromDb() : List<Register>
 }
