191acf5fb8c7322db1f98dc7241d98b3930e3a3d
Lucas Ferreira <lucasfteo@gmail.com>
2022-10-20 18:33:00 -0300

    Added the button that opens SeeRegistersActivity

--- app/src/main/java/com/example/cashflowacai/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/cashflowacai/ui/activity/MainActivity.kt
@@ -9,14 +9,22 @@ import com.example.cashflowacai.R
 class MainActivity : AppCompatActivity() {
 
     lateinit var btnForm : Button
+    lateinit var btnSeeRegiser : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnForm = findViewById(R.id.btnForm)
+        btnSeeRegiser = findViewById(R.id.btnSeeRegister)
 
         btnForm.setOnClickListener {
             val intent = Intent(this, FormActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSeeRegiser.setOnClickListener {
+            val intent = Intent(this, SeeRegistersActivity::class.java)
             startActivity(intent)
         }
     }
