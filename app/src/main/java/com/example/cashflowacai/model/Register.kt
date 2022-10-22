63b390beccb2106cd48bf2bec2ec8e5ed9c76178
Lucas Ferreira <lucasfteo@gmail.com>
2022-10-22 17:19:49 -0300

    Changed the model
    I was using date as String in my model but I was getting some difficulties to work whit the database this way, so I changed from String to Date.

--- app/src/main/java/com/example/cashflowacai/model/Register.kt
+++ app/src/main/java/com/example/cashflowacai/model/Register.kt
@@ -5,13 +5,14 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 import java.math.BigDecimal
+import java.util.*
 
 @Entity
 @Parcelize
 data class Register (
 
     @PrimaryKey(autoGenerate = true) val id: Long = 0L,
-    val date: String,
+    val date: Date,
     val pix: BigDecimal,
     val cash: BigDecimal,
     val debit: BigDecimal,
